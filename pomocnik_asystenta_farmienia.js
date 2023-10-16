216d1b9b834ffa82c58368db38fd7803a2d7fcae
Avensix <148070719+Avensix@users.noreply.github.com>
2023-10-16 14:31:07 +0200

    Update pomocnik_asystenta_farmienia.js

--- pomocnik_asystenta_farmienia.js
+++ pomocnik_asystenta_farmienia.js
@@ -1,40 +1,42 @@
+// Importujemy wymagane biblioteki
 const axios = require('axios');
 const cheerio = require('cheerio');
 
 const url = 'https://pl193.plemiona.pl/game.php?village=28111&screen=premium&mode=log';
 
-axios.get(url)
-  .then((response) => {
-    if (response.status === 200) {
-      const html = response.data;
-      const $ = cheerio.load(html);
+function getDataUsingSelectors(selectors) {
+  axios.get(url)
+    .then((response) => {
+      if (response.status === 200) {
+        const html = response.data;
+        const $ = cheerio.load(html);
 
-      // Znajdź odpowiednie selektory na stronie i wyciągnij dane
-      const data = [];
+        const data = [];
 
-      // Przykładowe selektory:
-      const dateSelector = $('YOUR_DATE_SELECTOR').text();
-      const worldSelector = $('YOUR_WORLD_SELECTOR').text();
-      const transactionSelector = $('YOUR_TRANSACTION_SELECTOR').text();
-      const changeSelector = $('YOUR_CHANGE_SELECTOR').text();
-      const newBalanceSelector = $('YOUR_NEW_BALANCE_SELECTOR').text();
-      const detailsSelector = $('YOUR_DETAILS_SELECTOR').text();
+        for (const selector of selectors) {
+          const value = $(selector).text();
+          data.push({
+            Selector: selector,
+            Value: value,
+          });
+        }
 
-      // Dodaj dane do tablicy
-      data.push({
-        Data: dateSelector,
-        Świat: worldSelector,
-        Transakcja: transactionSelector,
-        Zmiana: changeSelector,
-        NoweSaldoPP: newBalanceSelector,
-        DalszeInformacje: detailsSelector,
-      });
+        console.log(data);
+      }
+    })
+    .catch((error) => {
+      console.error('Wystąpił błąd podczas pobierania strony:', error);
+    });
+}
 
-      // Możesz przetwarzać dane, filtrować, sumować itp.
+function showDialogAndGetSelectors() {
+  const dialog = Dialog.show('Script', `
+    <div>
+      <p>Podaj selektory, które chcesz użyć do analizy strony.</p>
+      <textarea id="selectors" placeholder="Wprowadź selektory oddzielone przecinkami" style="width: 100%; height: 100px;"></textarea>
+      <button onclick="getDataUsingSelectors()">Pobierz dane</button>
+    </div>
+  `);
+}
 
-      console.log(data);
-    }
-  })
-  .catch((error) => {
-    console.error('Wystąpił błąd podczas pobierania strony:', error);
-  });
+showDialogAndGetSelectors();
